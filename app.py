e47b103403f43a5390227630dc77001d8ac01011
Abdul Salaam Adeleke <78124617+abduladeleke26@users.noreply.github.com>
2025-01-30 17:59:30 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,91 +1,76 @@
-import requests
 from flask import Flask, render_template, request, redirect, flash
+import requests
 import os
-urlt = "https://api-b2b.backenster.com/b1/api/v3/translate"
-urlc = "https://api-b2b.backenster.com/b1/api/v3/getLanguages?platform=api&code=en_GB"
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
+urltrans = "https://translate.googleapis.com/v3beta1/projects/929109150783:translateText"
+urldetect = "https://translate.googleapis.com/v3beta1/projects/929109150783:detectLanguage"
+urllan = "https://translate.googleapis.com/v3beta1/projects/929109150783/supportedLanguages"
 
-
-headersc = {
-        "accept": "application/json",
-        "Authorization": os.environ.get('FLASK_KEY')
+headers = {
+    "Authorization": f"Bearer {os.environ.get('FLASK_KEY')}",
+    "Content-Type": "application/json"
 }
 
-headerst = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "Authorization": os.environ.get('FLASK_KEY')
+
+def translatee(text,fromm,to):
+    if fromm != to:
+        payload = {
+            "contents": [text],
+            "targetLanguageCode": to,
+            "sourceLanguageCode": fromm,
+
+        }
+
+        response = requests.post(urltrans, json=payload, headers=headers)
+        gett = response.json()
+        translated = gett.get("translations")[0].get("translatedText")
+
+        return translated
+    else:
+        return text
+
+
+def getCode(text):
+    params = {
+        "displayLanguageCode": "en"
     }
 
+    response = requests.get(urllan, headers=headers, params=params)
 
+    languagess = response.json().get("languages")
+    for language in languagess:
+        name = language.get("displayName")
+        code = language.get("languageCode")
+        if text.lower() == name.lower():
+            return code, name
+        if text.lower() == code.lower():
+            return code, name
 
-def translator(word,to,fromm):
-    global urlt
-    global headerst
-
-    payload = {
-        "platform": "api",
-        "to": to,
-        "from": fromm,
-        "data": word
-    }
-
-    response = requests.post(urlt, json=payload, headers=headerst)
-
-    gett = response.json()
-
-    word = gett.get("result")
-    lan = gett.get("from")
-    return word, lan
-
-def getCountryCode(text):
-    global urlc
-    global headersc
-    global languages
-    response = requests.get(urlc, headers=headersc)
-
-    universe = response.json()
-    earth = universe.get("result")
-    for country in earth:
-        language = country['englishName']
-        alphaCode = country['code_alpha_1']
-        if language.lower() == text.lower() or alphaCode.lower() == text.lower() :
-            code = country['full_code']
-            return code, language, alphaCode
 
 def detect(text):
-    url = "https://api-gl.lingvanex.com/language/translate/v2/detect"
+    payload = {
 
-    payload = {"q": text}
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "Authorization": os.environ.get('FLASK_KEY')
+        "content": text
+
     }
 
-    response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(urldetect, json=payload, headers=headers)
 
-    dictt = response.json()
-    detections = dictt['data']
-    deeper = detections['detections']
-    evenDeeper = deeper[0]
-    evenEvenDeeper = evenDeeper[0]
-    language = evenEvenDeeper['language']
-    return language
-
-
-
-
+    gett = response.json()
+    language = gett.get("languages")[0].get("languageCode")
+    conf = gett.get("languages")[0].get("confidence")
+    return language, conf
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/translate',methods = ['POST'])
+
+@app.route('/translate', methods=['POST'])
 def translate():
     choice1 = request.form.get("choice1")
     choice2 = request.form.get("choice2")
@@ -102,32 +87,23 @@ def translate():
         return redirect('/')
 
     if choice1 == "Detect Language":
-        language = detect(text)
-        countryCode1, english1, code1 = getCountryCode(language)
-        choice1 = english1
+        language, conf = detect(text)
+        fromm, name = getCode(language)
     else:
-        countryCode1, english1, code1 = getCountryCode(choice1)
+        fromm, name = getCode(choice1)
 
+    language, conf = detect(text)
 
-    language = detect(text)
-    if language != code1:
-        countryCode1, english1, code1 = getCountryCode(language)
-        choice1 = english1
-    countryCode2, english2, code2 = getCountryCode(choice2)
+    conf *= 100
 
-    new, lan = translator(text,countryCode2,countryCode1)
+    if conf> 50:
+        fromm, name = getCode(language)
 
+    to, name2 = getCode(choice2)
 
+    translated = translatee(text, fromm, to)
 
-    return render_template("index.html", text = text, translated = new, choice1 = choice1, choice2 = choice2)
-
-
-
-
-
-
-
-
+    return render_template("index.html", text=text, translated=translated, choice1=name, choice2=name2)
 
 
 if __name__ == "__main__":
